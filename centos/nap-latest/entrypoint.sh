f0860c9461e2a2a972bcf9b0670ce393f7c1c4f8
Alexis <al.dacosta@f5.com>
2021-11-18 20:33:20 +0100

    debug

--- centos/nap-latest/entrypoint.sh
+++ centos/nap-latest/entrypoint.sh
@@ -132,9 +132,12 @@ wait_term()
 {
     wait ${agent_pid}
     trap - TERM
-    echo "-------- SIG TERM received"
-    export ENV_CONTROLLER_INSTANCE_NAME=${instance_name}
+    kill -QUIT "${nginx_pid}" 2>/dev/null
+    echo "waiting for nginx to stop..."
+    wait ${nginx_pid}
+    # unregister - start
     echo " UNREGISTER instance from Controller"
+    export ENV_CONTROLLER_INSTANCE_NAME=${instance_name}
     echo " ---> using ENV_CONTROLLER_API_URL = ${ENV_CONTROLLER_API_URL}"
     echo " ---> using ENV_CONTROLLER_USERNAME = ${ENV_CONTROLLER_USERNAME}"
     echo " ---> using ENV_CONTROLLER_PASSWORD = ${ENV_CONTROLLER_PASSWORD}"
@@ -142,9 +145,7 @@ wait_term()
     sh remove.sh
     echo " UNREGISTER done"
     sleep 60
-    kill -QUIT "${nginx_pid}" 2>/dev/null
-    echo "waiting for nginx to stop..."
-    wait ${nginx_pid}
+    # unregister - end
 }
 
 wait_term
